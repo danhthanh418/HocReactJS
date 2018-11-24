34e0244815b8bcf26dc51af4b2d30b5f188c67aa
Thanh Danh <15520798@gm.uit.edu.vn>
2018-11-24 10:42:50 +0700

    Search with child component

--- search-demo/src/App.js
+++ search-demo/src/App.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import Employees from './data/employees.json';
+import SearchBox from './components/SearchBox.js';
+import SearchResult from './components/SearchResult.js';
+// import SearchResult from './components/SearchResult.js';
 
 export default class App extends Component {
 
@@ -13,60 +16,29 @@ export default class App extends Component {
   };
   
 
-onChange  =(event)=>{
-  const target=event.target;
-  const value = target.value;
-  // return console.log(value)
-  this.setState({
-    term:value
-  })
-  console.log(this.state.term)
-}
-
-onSubmit= (event)=>{
-  event.preventDefault();
-  return console.log(event.target.value)
-}
-
-searchFor (term){
-  return (x)=>{
-    return x.preferredFullName.toLowerCase().includes(term.toLowerCase())||!term
+  onChange  =(event)=>{
+    this.setState({
+      term:event.target.value
+    })
   }
-}
+
+  onSubmit= (event)=>{
+    event.preventDefault();
+    return console.log(event.target.value)
+  }
   render () {
-    // console.log (this.state.items);
     const {items,term} = this.state;
     return (
       <form className="container" onSubmit={this.onSubmit}>
         <div className="form-group my-3">
-          <input
-            type="text"
-            name="filter"
-            id="filter"
-            className="form-control"
-            placeholder="Keyword to search"
-            aria-describedby="helpId"
-            onChange= {this.onChange}
-            value={term}
-          />
-          <div className="row m-2 ">
-            {items.filter(this.searchFor(term)).map ((employee, index) => {
-              return (
-                <div className="card col-3 m-2 p-1" key={index}>
-                  <div className="card-body p-2">
-                        <h4 className="card-title text-center text-primary">
-                          {employee.preferredFullName}
-                        </h4>
-                        <p className="card-text">Region :{employee.region}</p>
-                        <p className="card-text">Job name: {employee.jobTitleName}</p>
-                        <p className="card-text">Phone: {employee.phoneNumber}</p>
-                        <p className="card-text">Email: {employee.emailAddress}</p>
-                  </div>
-                </div>
-              );
-            })}
-
-          </div>
+         <SearchBox 
+            onChange={(e)=>this.onChange(e)}
+            term={term}
+         ></SearchBox>
+          <SearchResult 
+          items={items}
+          term={term}
+          ></SearchResult>
         </div>
       </form>
     );
